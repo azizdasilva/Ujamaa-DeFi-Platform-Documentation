9a1b3c98a06427f2a049efaf606d01f000ec9b3d
Aziz Da Silva <azis_dasilva@yahoo.com>
2026-04-09 21:13:32 +0100

    chore: move documentation to repository root

--- frontend/src/MVP/components/wallet/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Wallet Components Export
- * Ujamaa DeFi Platform - MVP Testnet
- */
-
-export { default as ConnectWallet } from './ConnectWallet';
-export { default as WalletStatus } from './WalletStatus';
-export { default as NetworkSwitcher } from './NetworkSwitcher';
